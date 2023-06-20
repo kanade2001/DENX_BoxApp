d05a1306b6522316a6caa5546496d6fb2af9b3b5
kanade2001 <kanade2001.cpp@gmail.com>
2023-06-21 01:11:06 +0900

    readNFC.pyが完成しました

--- readNFC.py
+++ readNFC.py
@@ -1,1 +1,13 @@
+import nfc
+import binascii
+
+def main():
+    clf = nfc.ContactlessFrontend('usb')
+    tag = clf.connect(rdwr={'targets': ['212F', '424F'], 'on-connect': lambda tag: False}) # Type3Tagを取得
+    idm = binascii.hexlify(tag.idm).decode() # 固有IDの抽出
+    return idm
+
+if __name__ == "__main__":
+    print(main())
+
 
