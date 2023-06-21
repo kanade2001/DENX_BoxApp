09acef2824411b3c0b0372030fc97e2f2194a88d
kanade2001 <kanade2001.cpp@gmail.com>
2023-06-21 22:58:43 +0900

    lcd.py

--- display.py
+++ display.py
@@ -1,2 +1,9 @@
+import lcd
+
 def main(line1, line2):
-    print(line1, line2)
+    l = lcd.HD44780()
+    l.string(line1, 1)
+    l.strint(line2, 2)
+    
+if __name__ == "__main__":
+    main("LCD Monitor", "sample123")
