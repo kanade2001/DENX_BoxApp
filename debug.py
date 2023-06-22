f33eaa36c91542b60f11bb9607a5c25fa97c11f9
kanade2001 <kanade2001.cpp@gmail.com>
2023-06-23 02:27:42 +0900

    修正

--- debug.py
+++ debug.py
@@ -5,6 +5,6 @@ import time
 def main():
     display.update("DEBUG MODE", "Touch NFC Tag")
     idm = readNFC.main()
-    display.update("Your idm is", str(idm),waittime=10)
+    display.update("Your idm is", str(idm))
     time.sleep(10)
     display.update(None, None)
--- display.py
+++ display.py
@@ -8,15 +8,17 @@ showlimit = time.time()
 display = drivers.Lcd()
 
 def main():
-    if str1 == None:
-        str1 = "DENX BOX 233"
-    if str2 ==None:
-        str2 = str(datetime.datetime.now().date())[5:] + " " + str(datetime.datetime.now().time().isoformat(timespec='seconds'))
-    display.lcd_display_string(str1, 1)
-    display.lcd_display_string(str2, 2)
+    display.lcd_display_string(str1 if str1 != None else "DENX BOX 233   ", 1)
+    display.lcd_display_string(str2 if str2 != None else str(datetime.datetime.now().date())[5:] + " " + str(datetime.datetime.now().time().isoformat(timespec='seconds')) + "  ", 2)
 
 def update(line1, line2):
     global str1
     global str2
-    str1 = line1
-    str2 = line2
+    if line1 == None:
+        str1 = None
+    else:
+        str1 = line1.ljust(16)
+    if line2 == None:
+        str2 = None
+    else:
+        str2 = line2.ljust(16)
